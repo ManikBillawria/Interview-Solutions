886c3ce714906a5d8b79d2a1845b2dd484b15235
Manik Billawria <smbillawria@gmail.com>
2022-04-27 00:53:13 +0530

    Time: 1 ms (78.69%), Space: 43.3 MB (49.19%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,28 +1,18 @@
 class Solution {
-    
-    void subsetsUtil(int[] nums, int i, int n, int setSize, List<Integer> set, List<List<Integer>> res) {
-        if(i == n) {
-            for(int k = 0; k < setSize; k++) {
-                System.out.print(set.get(k)+" ");
-            }
-            System.out.println();
-            res.add(new ArrayList<>(set));
-            return;
-        }
-        subsetsUtil(nums, i+1, n, setSize, set, res);
-        
-        set.add(setSize, nums[i]);
-        subsetsUtil(nums, i+1, n, setSize+1, set, res);
-        
-        set.remove(set.size()-1);
-    }
-    
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        List<Integer> set = new ArrayList<Integer>();
         int n = nums.length;
+        int totalSubsets = (1<<n); // 1<<n = 2^n
         
-        subsetsUtil(nums, 0, n, 0, set, res);
+        for(int mask = 0; mask < totalSubsets; mask++) {
+            List<Integer> subset = new ArrayList<Integer>();
+            for(int i = 0; i < n; i++) {
+                if((mask & (1<<i)) != 0) {
+                    subset.add(nums[i]);
+                }
+            }
+            res.add(subset);
+        }
         return res;
     }
 }
